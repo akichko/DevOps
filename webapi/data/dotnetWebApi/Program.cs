747a3fd7a801879e921e30ccd32305795caf47cf
akichko <s.akihiko@gmail.com>
2023-04-03 07:47:48 +0900

    refactoring

--- webapi/data/dotnetWebApi/Program.cs
+++ webapi/data/dotnetWebApi/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -20,8 +22,14 @@ if (app.Environment.IsDevelopment())
 
 app.UseAuthorization();
 
-app.MapControllers();
+//app.UseStaticFiles(new StaticFileOptions
+//{
+//    FileProvider = new PhysicalFileProvider(
+//           Path.Combine(builder.Environment.ContentRootPath, "/workspace/app/publish/wwwroot")),
+//    RequestPath = ""
+//});
 
+app.MapControllers();
 
 app.UseDefaultFiles(); // デフォルトページを有効化
 app.UseStaticFiles(); // 静的ファイルを提供するミドルウェアを有効化
--- webapi/data/dotnetWebApi/deploy.sh
+++ webapi/data/dotnetWebApi/deploy.sh
@@ -1,2 +1,3 @@
 rm -rf /workspace/app/publish
-cp -r /workspace/dotnetWebApi/bin/Release/net6.0/publish /workspace/app/publish
+cp -r /workspace/dotnetWebApi/bin/Release/net6.0/publish /workspace/app/
+cp -r /workspace/dotnetWebApi/wwwroot /workspace/app/publish/
